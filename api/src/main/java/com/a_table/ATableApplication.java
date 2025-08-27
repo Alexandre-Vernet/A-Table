f60d53d91909962bae78ea39f6267c9a9edef07e
Alexandre Vernet <alexandre.vernet99@gmail.com>
2025-08-27 18:58:00 +0200

    Add back-end

---
+++ api/src/main/java/com/a_table/ATableApplication.java
@@ -0,0 +1,13 @@
+package com.a_table;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ATableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ATableApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/a_table/ATableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a_table;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ATableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
