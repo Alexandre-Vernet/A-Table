2983654489933e88e49cc011abed7760091cc286
Alexandre Vernet <alexandre.vernet99@gmail.com>
2025-09-05 19:56:39 +0200

    fix(recipe): Add @Transactional annotation to getRecipes method for session management

--- api/src/main/java/com/a_table/controller/RecipeController.java
+++ api/src/main/java/com/a_table/controller/RecipeController.java
@@ -6,6 +6,7 @@ import com.a_table.exception.InvalidCategoryException;
 import com.a_table.service.RecipeService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ public class RecipeController {
     RecipeService recipeService;
 
     @GetMapping("/")
+    @Transactional(readOnly = true) // <-- Garde la session ouverte
     List<Recipe> getRecipes() {
         return recipeService.getRecipes();
     }
