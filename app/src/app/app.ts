a680e5c45649a7b2845c3b3c6e3cf6b512ababb6
Alexandre Vernet <alexandre.vernet99@gmail.com>
2025-09-05 17:21:50 +0200

    feat(app): Implement service worker updates and alerts for PWA support

--- app/src/app/app.ts
+++ app/src/app/app.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AlertComponent } from "./shared/alert/alert.component";
+import { SwPush, SwUpdate } from "@angular/service-worker";
+import { PrimeNG } from "primeng/config";
+import { environment } from "../environments/environment";
 
 @Component({
     selector: 'app-root',
@@ -8,5 +11,39 @@ import { AlertComponent } from "./shared/alert/alert.component";
     templateUrl: './app.html',
     styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
+
+    constructor(
+        private readonly sw: SwPush,
+        private readonly swUpdate: SwUpdate,
+        private primeng: PrimeNG,
+    ) {
+        if (environment.production) {
+            // Force refresh PWA
+            this.swUpdate.checkForUpdate();
+            if (this.swUpdate.isEnabled) {
+                this.swUpdate.versionUpdates.subscribe(event => {
+                    if (event.type === 'VERSION_READY') {
+                        window.location.reload();
+                    }
+                });
+            }
+
+            if (!('serviceWorker' in navigator)) {
+                setTimeout(() => {
+                    alert('Service workers are not supported in this browser');
+                }, 1000);
+            }
+
+            if (!this.sw.isEnabled) {
+                setTimeout(() => {
+                    alert('Service workers are not enabled');
+                }, 1000);
+            }
+        }
+    }
+
+    ngOnInit() {
+        this.primeng.ripple.set(true);
+    }
 }
