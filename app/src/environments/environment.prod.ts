ffb1418f3fd6e1762a1ddabdf09c47b987497759
Alexandre Vernet <alexandre.vernet99@gmail.com>
2025-09-05 17:33:17 +0200

    fix(environment): Update recipe URL to remove trailing slash

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     API_URL: 'https://a-table-api.alexandre-vernet.fr/api',
-    recipeUrl: () => `${ environment.API_URL }/recipe/`,
+    recipeUrl: () => `${ environment.API_URL }/recipe`,
 };
