9825e0af6dc1fd107250e70508731e8e14b579b3
Alexandre Vernet <alexandre.vernet99@gmail.com>
2025-09-08 17:10:05 +0200

    feat(view-recipe): Add delete functionality with confirmation dialog

--- app/src/app/recipe/view-recipe/view-recipe.ts
+++ app/src/app/recipe/view-recipe/view-recipe.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RecipeService } from '../../services/recipe.service';
 import { switchMap } from 'rxjs';
 import { Recipe } from '../../dto/Recipe';
@@ -7,6 +7,9 @@ import { TitleCasePipe } from '@angular/common';
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from 'primeng/tabs';
 import { TimeConvertPipe } from "../../pipes/time-convert-pipe";
 import { AlertService } from "../../services/alert.service";
+import { Button } from "primeng/button";
+import { ConfirmDialog } from "primeng/confirmdialog";
+import { ConfirmationService } from "primeng/api";
 
 @Component({
     selector: 'app-view-recipe',
@@ -17,11 +20,14 @@ import { AlertService } from "../../services/alert.service";
         Tab,
         TabPanels,
         TabPanel,
-        TimeConvertPipe
+        TimeConvertPipe,
+        Button,
+        ConfirmDialog
     ],
     templateUrl: './view-recipe.html',
     styleUrl: './view-recipe.scss',
-    standalone: true
+    standalone: true,
+    providers: [ConfirmationService]
 })
 export class ViewRecipe implements OnInit {
 
@@ -29,8 +35,10 @@ export class ViewRecipe implements OnInit {
 
     constructor(
         private readonly route: ActivatedRoute,
+        private readonly router: Router,
         private readonly recipeService: RecipeService,
-        private readonly alertService: AlertService
+        private readonly alertService: AlertService,
+        private readonly confirmationService: ConfirmationService
     ) {
     }
 
@@ -44,5 +52,44 @@ export class ViewRecipe implements OnInit {
                     message: err?.error?.message ?? 'Impossible de supprimer cette recette'
                 }))
             });
+    }
+
+    showDialogDeleteRecipe(recipe: Recipe) {
+        this.confirmationService.confirm({
+            target: event.target as EventTarget,
+            message: 'Voulez-vous vraiment supprimer cette recette ?',
+            header: 'Supprimer la recette',
+            icon: 'pi pi-info-circle',
+            rejectLabel: 'Cancel',
+            rejectButtonProps: {
+                label: 'Cancel',
+                severity: 'secondary',
+                outlined: true,
+            },
+            acceptButtonProps: {
+                label: 'Delete',
+                severity: 'danger',
+            },
+
+            accept: () => this.deleteRecipe(recipe),
+        });
+    }
+
+    deleteRecipe(recipe: Recipe) {
+        this.recipeService.deleteRecipe(recipe)
+            .subscribe({
+                next: () => {
+                    this.router.navigate(['/']);
+
+                    this.alertService.alert$.next({
+                        severity: 'success',
+                        message: 'Recette supprimée avec succès'
+                    });
+                },
+                error: (err => this.alertService.alert$.next({
+                    severity: 'error',
+                    message: err?.error?.message ?? 'Impossible de supprimer cette recette'
+                }))
+            })
     }
 }
