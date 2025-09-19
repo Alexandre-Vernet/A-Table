8c94530e6df2b499bd5b964de2afacadc383cb02
Alexandre Vernet <72151831+Alexandre-Vernet@users.noreply.github.com>
2025-09-19 21:30:40 +0200

    feat(menu): Add menu (#55)

--- app/src/app/services/user.service.ts
+++ app/src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { User } from '../dto/User';
-import { of, tap } from 'rxjs';
+import { BehaviorSubject, of, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { AlertService } from './alert.service';
 
@@ -11,6 +11,9 @@ import { AlertService } from './alert.service';
 export class UserService {
 
     userUrl = environment.userUrl();
+
+    private user = new BehaviorSubject<User>(null);
+    user$ = this.user.asObservable();
 
     constructor(
         private readonly http: HttpClient,
@@ -23,6 +26,7 @@ export class UserService {
             return this.http.get<User>(`${ this.userUrl }/me`)
                 .pipe(
                     tap(user => user ? this.alertService.clear() : null),
+                    tap(user => this.user.next(user)),
                 );
         }
         return of(null);
@@ -62,6 +66,7 @@ export class UserService {
 
 
     signOut() {
+        this.user.next(null);
         localStorage.removeItem('token');
     }
 }
